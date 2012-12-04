495f8bc70d3276353fe4f455fc13122f3e79c066
Nagaprasad N <gorbi16@gmail.com>
2012-12-04 16:47:08 +0530

    initial upload of device tree based on 3.14.401.22

---
+++ BoardConfig.mk
@@ -0,0 +1,108 @@
+# Copyright 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# inherit from the proprietary version
+-include vendor/htc/endeavoru/BoardConfigVendor.mk
+
+TARGET_BOARD_PLATFORM := tegra
+
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+ARCH_ARM_HAVE_TLS_REGISTER := true
+ARCH_ARM_USE_NON_NEON_MEMCPY := true
+
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := 
+
+USE_OPENGL_RENDERER := true
+BOARD_EGL_CFG := device/htc/endeavoru/config/egl.cfg
+
+BOARD_KERNEL_CMDLINE := 
+BOARD_KERNEL_PAGESIZE := 2048
+
+TARGET_USERIMAGES_USE_EXT4 := true
+
+# Partitions Info
+#cat /proc/emmc
+#dev:        size     erasesize name
+#mmcblk0p5: 00800000 00001000 "recovery"
+#mmcblk0p4: 00800000 00001000 "boot"
+#mmcblk0p12: 50000000 00001000 "system"
+#mmcblk0p13: 14000000 00001000 "cache"
+#mmcblk0p17: 00200000 00001000 "misc"
+#mmcblk0p1: 00600000 00001000 "wlan"
+#mmcblk0p2: 00200000 00001000 "WDM"
+#mmcblk0p20: 00200000 00001000 "pdata"
+#mmcblk0p3: 00600000 00001000 "radiocab"
+#mmcblk0p14: 650000000 00001000 "internalsd"
+#mmcblk0p15: 89400000 00001000 "userdata"
+#mmcblk0p19: 01600000 00001000 "devlog"
+#mmcblk0p16: 00200000 00001000 "extra"
+
+BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8388608
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1342177280
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2302672896
+BOARD_FLASH_BLOCK_SIZE := 4096
+
+BOARD_VOLD_MAX_PARTITIONS := 20
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/fsl-tegra-udc/gadget/lun0/file
+
+# Wifi related defines
+USES_TI_MAC80211 := true
+ifdef USES_TI_MAC80211
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wl12xx
+BOARD_WLAN_DEVICE                := wl12xx_mac80211
+BOARD_SOFTAP_DEVICE              := wl12xx_mac80211
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wl12xx_sdio.ko"
+WIFI_DRIVER_MODULE_NAME          := "wl12xx_sdio"
+WIFI_FIRMWARE_LOADER             := ""
+COMMON_GLOBAL_CFLAGS             += -DUSES_TI_MAC80211
+endif
+
+TARGET_PREBUILT_KERNEL := device/htc/endeavoru/kernel
+
+# Avoid the generation of ldrcc instructions
+NEED_WORKAROUND_CORTEX_A9_745320 := true
+
+TARGET_PROVIDES_LIBAUDIO := true
+BOARD_USES_GENERIC_AUDIO := false
+BOARD_USES_ALSA_AUDIO := false
+
+BOARD_USE_NEW_LIBRIL_HTC := true
+
+COMMON_GLOBAL_CFLAGS += -DHTCLOG
+
+BOARD_USES_GENERIC_INVENSENSE := false
+
+BOARD_HAVE_BLUETOOTH := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/endeavoru/bluetooth
+
+BOARD_HAS_NO_SELECT_BUTTON := true
+# Use this flag if the board has a ext4 partition larger than 2gb
+BOARD_HAS_LARGE_FILESYSTEM := true
